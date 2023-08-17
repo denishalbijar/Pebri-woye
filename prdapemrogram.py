eb707f3670520877246dde1498bcb9710dcf8b81
Dragoon <140218606+denishalbijar@users.noreply.github.com>
2023-08-17 18:42:51 +0700

    Add files via upload

---
+++ prdapemrogram.py
@@ -0,0 +1,3 @@
+print("Nama:Yaisy fathurrohman")
+print("Kelas:XRPL1")
+print("Alasan masuk Rpl:ingin menjadi developer aplikasi dan game ingin membuat aplikasi yang bisa membantu semua orang dan membuat game yang seru")
